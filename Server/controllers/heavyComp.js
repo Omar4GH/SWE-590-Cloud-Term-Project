46f85290f74828582778f131488fa726791b1d3f
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-12-28 01:16:59 +0300

    Update heavyComp.js

--- Server/controllers/heavyComp.js
+++ Server/controllers/heavyComp.js
@@ -23,11 +23,13 @@ const primeFactorization = (number) => {
 // Endpoint for heavy computation (prime factorization)
 export const getHeavyComputation = async (req, res) => {
   const { number } = req.body;
-
+  console.log('Received number:', number);
   try {
     const factors = primeFactorization(number);
     console.log("Done Calculating")
-    res.status(200).json({ factors });
+    const factorsString = factors.join(', ');
+
+    res.status(200).json({ factors: factorsString });
   } catch (error) {
     console.error('Error performing heavy computation:', error);
     res.status(500).json({ error: 'Error performing heavy computation' });
